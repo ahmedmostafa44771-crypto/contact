8f0a935ad293dc18248f6797bd8bb7f9e6f51184
ahmedmostafa44771-crypto <ahmedmostafa44771@gmail.com>
2025-12-24 21:52:56 +0200

    added home screen

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,6 @@
+import 'package:contact/utils/app_assets.dart';
+import 'package:contact/utils/app_colors.dart';
+import 'package:contact/utils/app_style.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,7 +11,42 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.darkBlue,
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(AppAsset.squares),
+                const SizedBox(height: 16),
+                Text(
+                  'There is No Contacts Added Here',
+                  style: AppStyles.goldMedium20,
+                ),
+              ],
+            ),
+          ),
 
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: Container(
+              width: 56,
+              height: 56,
+              decoration: BoxDecoration(
+                color: AppColors.gold,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.add),
+                color: AppColors.darkBlue,
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
