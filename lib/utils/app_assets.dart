0282459cb832a0d56bfc1f54203d2ec69df024bc
ahmedmostafa44771-crypto <ahmedmostafa44771@gmail.com>
2025-12-24 20:12:34 +0200

    Added base images - styles - colors

--- lib/utils/app_assets.dart
+++ lib/utils/app_assets.dart
@@ -2,4 +2,5 @@ abstract final class AppAsset {
   static const _basePath = "assets/image/";
   static const earth = "${_basePath}splash_screen.png";
   static const icon = "${_basePath}FAB.png";
+  static const squares = "${_basePath}list-purple-Xetxuqguwn.png";
 }
